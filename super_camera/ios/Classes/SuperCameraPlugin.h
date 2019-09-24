233a2ed5596d514344bcbc4de65f72f46eb239ca
Maurice Parrish <bmparr@google.com>
2019-09-24 16:30:50 -0700

    Initial creation of super_camera

---
+++ super_camera/ios/Classes/SuperCameraPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface SuperCameraPlugin : NSObject<FlutterPlugin>
+@end
---
+++ super_camera/ios/Classes/SuperCameraPlugin.m
@@ -0,0 +1,20 @@
+#import "SuperCameraPlugin.h"
+
+@implementation SuperCameraPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"super_camera"
+            binaryMessenger:[registrar messenger]];
+  SuperCameraPlugin* instance = [[SuperCameraPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
