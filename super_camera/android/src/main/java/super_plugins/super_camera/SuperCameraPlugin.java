d24450cebd19c8bad1d4425b45db25d42e4b7b7f
Maurice Parrish <bmparr@google.com>
2019-11-05 19:32:03 -0800

    Add camerax to proj

--- super_camera/android/src/main/java/super_plugins/super_camera/SuperCameraPlugin.java
+++ super_camera/android/src/main/java/super_plugins/super_camera/SuperCameraPlugin.java
@@ -1,18 +1,26 @@
 package super_plugins.super_camera;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleOwner;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** SuperCameraPlugin */
-public class SuperCameraPlugin {
+public class SuperCameraPlugin implements FlutterPlugin, ActivityAware, LifecycleOwner {
   private static final String CHANNEL_NAME = "super_plugins/super_camera";
   private static final String PLATFORM_VIEW_FACTORY_NAME = CHANNEL_NAME + "/views";
   private static final String TEXTURE_REGISTRY_ID = "texture_registry";
 
+  private ActivityPluginBinding activityBinding;
+  private FlutterPluginBinding flutterBinding;
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(),
-        "super_plugins/super_camera");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
     final ChannelGenerated channelGenerated = new ChannelGenerated();
     channel.setMethodCallHandler(channelGenerated);
 
@@ -26,5 +34,53 @@ public class SuperCameraPlugin {
         .registerViewFactory(
             PLATFORM_VIEW_FACTORY_NAME,
             channelGenerated.getPlatformViewFactory());
+  }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding binding) {
+    flutterBinding = binding;
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    activityBinding = binding;
+    final MethodChannel channel = new MethodChannel(flutterBinding.getBinaryMessenger(), CHANNEL_NAME);
+    final ChannelGenerated channelGenerated = new ChannelGenerated();
+    channel.setMethodCallHandler(channelGenerated);
+
+    final ChannelGenerated.TextureRegistryWrapper activityWrapper = new ChannelGenerated.TextureRegistryWrapper(
+        channelGenerated,
+        TEXTURE_REGISTRY_ID,
+        flutterBinding.getTextureRegistry());
+    channelGenerated.addAllocatedWrapper(TEXTURE_REGISTRY_ID, activityWrapper);
+    flutterBinding.getPlatformViewRegistry().registerViewFactory(
+        PLATFORM_VIEW_FACTORY_NAME,
+        channelGenerated.getPlatformViewFactory());
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    activityBinding = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    activityBinding = binding;
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    activityBinding = null;
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+    flutterBinding = null;
+  }
+
+  @NonNull
+  @Override
+  public Lifecycle getLifecycle() {
+    return (Lifecycle) activityBinding.getLifecycle();
   }
 }
